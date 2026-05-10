f6859d565a79602764402f42956aca1f4feeea3a
PepeJEJEJ <jah0005@alu.medac.es>
2026-05-10 11:55:18 +0200

    234
    142

--- Codigos/B8/II/Main.java
+++ Codigos/B8/II/Main.java
@@ -6,16 +6,19 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String Nombre = sc.nextLine();
-        Double Precio = sc.nextDouble();
-        int Stock = sc.nextInt();
         try {
+            System.out.println("Pon Nombre");
+            String Nombre = sc.nextLine();
+            System.out.println("Pon Precio");
+            Double Precio = sc.nextDouble();
+            System.out.println("Pon Stock");
+            int Stock = sc.nextInt();
             BufferedWriter bw = new BufferedWriter(new FileWriter("inventario.csv", true));
             bw.write(Nombre + "," + Precio + "," + Stock);
             bw.newLine();
             bw.close();
         } catch (Exception e) {
-            System.out.println("ERROR"+e);
+            System.out.println("ERROR" + e);
         }
     }
 }
