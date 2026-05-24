d978c6e0630aa4903981ce50a1a3d5907ea349fa
PepeJEJEJ <jah0005@alu.medac.es>
2026-05-24 10:12:41 +0200

    aethelings's lands
    essex
    sussex
    wessex
    norsex

--- RepasosRecuperacion/Tema 9/Cinco.java
+++ RepasosRecuperacion/Tema 9/Cinco.java
@@ -2,33 +2,37 @@ import javax.swing.*;
 
 public class Cinco {
     public static void main(String[] args) {
-        double cambio=0.80;
-        JFrame ventana = new JFrame("Por el qlo te la hinco, Conversor Euros/Dólares");
-        ventana.setSize(300, 150);
-        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        try {
+            double cambio = 0.80;
+            JFrame ventana = new JFrame("Por el qlo te la hinco, Conversor Euros/Dólares");
+            ventana.setSize(300, 150);
+            ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel panel = new JPanel();
-        ventana.add(panel);
+            JPanel panel = new JPanel();
+            ventana.add(panel);
 
-        JTextField campo = new JTextField(10);
-        panel.add(campo);
+            JTextField campo = new JTextField(10);
+            panel.add(campo);
 
-        JLabel resultado = new JLabel("Resultado");
-        panel.add(resultado);
+            JLabel resultado = new JLabel("Resultado");
+            panel.add(resultado);
 
-        JButton ConEuro = new JButton("Dolares=>Euros");
-        panel.add(ConEuro);
-        JButton ConDolares = new JButton("Euros=>Dolares");
-        panel.add(ConDolares);
+            JButton ConEuro = new JButton("Dolares=>Euros");
+            panel.add(ConEuro);
+            JButton ConDolares = new JButton("Euros=>Dolares");
+            panel.add(ConDolares);
 
-        ConEuro.addActionListener(e -> {
-            Double valor=Double.parseDouble(campo.getText());
-            resultado.setText(""+(valor*cambio));
-        });
-        ConDolares.addActionListener(e -> {
-            Double valor=Double.parseDouble(campo.getText());
-            resultado.setText(""+(valor/cambio));
-        });
-        ventana.setVisible(true);
+            ConEuro.addActionListener(e -> {
+                Double valor = Double.parseDouble(campo.getText());
+                resultado.setText("" + (valor * cambio));
+            });
+            ConDolares.addActionListener(e -> {
+                Double valor = Double.parseDouble(campo.getText());
+                resultado.setText("" + (valor / cambio));
+            });
+            ventana.setVisible(true);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
