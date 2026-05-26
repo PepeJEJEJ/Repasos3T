9b911fcde08adeb8229508755b41ec16a37601bb
PepeJEJEJ <jah0005@alu.medac.es>
2026-05-26 20:16:01 +0200

    fsa
    af

--- RepasoCero/B10/Ejercicio1/src/App.java
+++ RepasoCero/B10/Ejercicio1/src/App.java
@@ -5,13 +5,15 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int opcion = 0;
-        // EL MENU
         do {
             System.out.println("1. Listado basico");
             System.out.println("2. Buscar por curso");
             System.out.println("3. Media por curso");
             System.out.println("4. Aprobados/Suspensos");
             System.out.println("5. Mejor estudiante");
+            System.out.println("6. Nueva matrícula");
+            System.out.println("7. Actualizar nota tras la recuperación o subida de nota");
+            System.out.println("8. Baja de un alumno");
             System.out.println("0. Chau");
 
             opcion = sc.nextInt();
@@ -22,16 +24,16 @@ public class App {
                     mostrarListadoBasico();
                     break;
                 case 2:
-                    BuscarCurso();
+                    BuscarporCurso();
                     break;
                 case 3:
-                    MediaCursos();
+                    CalcularMediaCurso();
                     break;
                 case 4:
-                    AprobSus();
+                    AprobadosSuspensos();
                     break;
                 case 5:
-                    SpesialOne();
+                    TopEstudiante();
                     break;
                 case 6:
                     Matricular();
@@ -40,61 +42,54 @@ public class App {
                     actualizarNota();
                     break;
                 case 8:
-                    DarBaja();
+                    Baja();
+                    break;
+                default:
                     break;
             }
 
         } while (opcion != 0);
     }
 
-    /*
-     * 1 Listado básico: la primera opción del menú será mostrar todo el listado de
-     * alumnos completo, pero solo mostramos nombre y curso, no mostramos la nota.
-     */
     public static void mostrarListadoBasico() {
-        String url = "jdbc:mysql://localhost:3306/instituto";//LA URL PA CONECTARNOS
-        String user = "root";//EL USUARIO CON EL QUE VAMOS A CONECTARNOS
-        String password = "Med@c";//LA CONTRASEÑA
+        String url = "jdbc:mysql://localhost:3306/instituto";
+        String user = "root";
+        String password = "12345";
 
         String sql = "SELECT nombre, curso FROM alumnos";
 
         try {
-            Connection conn = DriverManager.getConnection(url, user, password);// PILLA LAS CREDENCIALES
-            PreparedStatement stmt = conn.prepareStatement(sql);// LA ACCION SQL
-            ResultSet rs = stmt.executeQuery();// EJECUTAR LA ACCION SQL
+            Connection conn = DriverManager.getConnection(url, user, password);
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
 
-            while (rs.next()) {// CURSOR QUE AVANZA MIENTRAS DEVUELVAN INFO
-                String nombre = rs.getString("nombre");// SI HAY UN NOMBRE, DEVUELVE EL NOMBRE
-                String curso = rs.getString("curso");// SI HAY CURSO, TAMBIEN LO DEVUELVE
+            while (rs.next()) {
+                String nombre = rs.getString("nombre");
+                String curso = rs.getString("curso");
                 System.out.println(nombre + " - " + curso);
             }
 
-            rs.close();// CERRAR TODO
+            rs.close();
             stmt.close();
             conn.close();
 
-        } catch (SQLException e) {// EXCEPCIONES
-            System.out.println("Erorr de SQL " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Error de SQL " + e.getMessage());
         } catch (Exception e) {
             System.out.println("Error desconocido" + e.getMessage());
         }
     }
 
-    /*
-     * 2 Buscar por curso: la segunda opción del menú consiste en solicitar al
-     * usuario
-     * un curso, y mostrar todos los alumnos de dicho curso.
-     */
-    public static void BuscarCurso() {
+    public static void BuscarporCurso() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Mete el curso que vamos a buscar: ");
         String cursoBuscado = sc.nextLine();
 
         String url = "jdbc:mysql://localhost:3306/instituto";
         String user = "root";
-        String password = "Med@c";
+        String password = "12345";
 
-        String sql = "SELECT nombre, curso FROM alumnos WHERE curso = ?"; // "?" es un parametro (va a ser reemplazado)
+        String sql = "SELECT nombre, curso FROM alumnos where curso=?";
 
         try {
             Connection conn = DriverManager.getConnection(url, user, password);
@@ -120,32 +115,28 @@ public class App {
         }
     }
 
-    /*
-     * 3. Calcular la media de cada curso: la tercera opción del menú primero
-     * pregunta al usuario de qué curso quiere saber la nota. Cuando el usuario haya
-     * especificado un curso, el programa muestra la nota media de todos los alumnos
-     * del curso indicado.
-     */
-    public static void MediaCursos() {
+    public static void CalcularMediaCurso() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Dame el curso: ");
+        System.out.print("Mete el curso que vamos a buscar: ");
         String cursoBuscado = sc.nextLine();
 
         String url = "jdbc:mysql://localhost:3306/instituto";
         String user = "root";
-        String password = "Med@c";
+        String password = "12345";
 
-        String sql = "SELECT AVG(nota_media) AS media FROM alumnos WHERE curso = ?";
+        String sql = "SELECT avg(nota_media) as Media FROM alumnos where curso=?";
+
         try {
             Connection conn = DriverManager.getConnection(url, user, password);
             PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
             stmt.setString(1, cursoBuscado);
 
-            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                String nombre = rs.getString("nombre");
+                String curso = rs.getString("curso");
+                System.out.println(nombre + " - " + curso);
 
-            if (rs.next()) {
-                double media = rs.getDouble("media");// ESTO ES LA MEDIA, ESTO GENERA UNA COLUMNA LLAMADA "MEDIA"
-                System.out.println("Media del curso " + cursoBuscado + ": " + media); // DEVUELVE UNA SOLA FILA
             }
 
             rs.close();
@@ -153,35 +144,29 @@ public class App {
             conn.close();
 
         } catch (SQLException e) {
-            System.out.println("Erorr de SQL " + e.getMessage());
+            System.out.println("Error de SQL " + e.getMessage());
         } catch (Exception e) {
             System.out.println("Error desconocido" + e.getMessage());
         }
     }
 
-    /*
-     * 4. Aprobados o suspensos: la cuarta opción del menú pregunta al usuario si
-     * quiere ver la lista de todos los aprobados o la lista de todos los suspensos,
-     * y una vez conteste el usuario, se muestra la lista correspondiente (de ambos
-     * cursos).
-     */
-    public static void AprobSus() {
+    public static void AprobadosSuspensos() {
         Scanner sc = new Scanner(System.in);
         System.out.print("A aprobados o S suspensos: ");
         String opcion = sc.nextLine().toUpperCase();
 
         String url = "jdbc:mysql://localhost:3306/instituto";
         String user = "root";
-        String password = "Med@c";
+        String password = "12345";
 
-        String sql = "";// EL COMANDO SQL EN BLANCO (EL PQ ESTA ABAJO:)
+        String sql = "";
 
-        if (opcion.equals("A")) {// A (POR TECLAO) NOS DA LOS APROBADOS (SI SUS NOTAS SUPERAN O SON 5)
+        if (opcion.equals("A")) {
             sql = "SELECT nombre, curso, nota_media FROM alumnos WHERE nota_media >= 5";
-        } else if (opcion.equals("S")) {// B (POR TECLAO) NOS DA LOS SUSPENSOS (SI SUS NOTAS NO SUPERAN NI EL 5)
+        } else if (opcion.equals("S")) {
             sql = "SELECT nombre, curso, nota_media FROM alumnos WHERE nota_media < 5";
         } else {
-            System.out.println("Opcion invalida");// SI PONES BABOSADAS, SALE ESTO
+            System.out.println("Opcion invalida");
             return;
         }
 
@@ -190,11 +175,11 @@ public class App {
             PreparedStatement stmt = conn.prepareStatement(sql);
             ResultSet rs = stmt.executeQuery();
 
-            while (rs.next()) {// LAS COLUMNAS PA MOSTRAR
+            while (rs.next()) {
                 String nombre = rs.getString("nombre");
                 String curso = rs.getString("curso");
-                double nota = rs.getDouble("nota");
-                System.out.println(nombre + " - " + curso + " - Nota: " + nota);
+                double nota_media = rs.getDouble("nota_media");
+                System.out.println(nombre + " - " + curso + " - Nota Media: " + nota_media);
             }
 
             rs.close();
@@ -208,25 +193,19 @@ public class App {
         }
     }
 
-    /*
-     * 5 El mejor estudiante: la quinta opción del menú consiste en mostrar el mejor
-     * estudiante de todos
-     */
-    public static void SpesialOne() {
-        System.out.print("VER AL TOP DE TOPS: ");
-
+    public static void TopEstudiante() {
         String url = "jdbc:mysql://localhost:3306/instituto";
         String user = "root";
-        String password = "Med@c";
+        String password = "12345";
 
-        String sql = "Select nombre, curso, nota_media FROM alumnos order by nota_media desc limit 1";// EL 1º REGISTRO TIENE LA NOTA MAS ALTA
+        String sql = "Select nombre, curso, nota_media FROM alumnos order by nota_media desc limit 1";
 
         try {
             Connection conn = DriverManager.getConnection(url, user, password);
             PreparedStatement stmt = conn.prepareStatement(sql);
             ResultSet rs = stmt.executeQuery();
 
-            while (rs.next()) {// LAS COLUMNAS PA MOSTRAR SIEMPRE QUE HAYA INFORMACION
+            while (rs.next()) {
                 String nombre = rs.getString("nombre");
                 String curso = rs.getString("curso");
                 double nota_media = rs.getDouble("nota_media");
@@ -240,16 +219,12 @@ public class App {
         } catch (SQLException e) {
             System.out.println("Error de SQL " + e.getMessage());
         } catch (Exception e) {
-            System.out.println("Error desconocido " + e.getMessage());
+            System.out.println("Error desconocido" + e.getMessage());
         }
     }
 
-    /*
-     * 6 Nueva matrícula: esta sexta opción del menú consiste en solicitar todos los
-     * datos de un nuevo estudiante y matricularlo (añadirlo a la bbdd).
-     */
     public static void Matricular() {
-        Scanner sc = new Scanner(System.in);// VALORES PA PONER
+        Scanner sc = new Scanner(System.in);
         System.out.print("Nombre del alumno: ");
         String nombre = sc.nextLine();
         System.out.print("Curso del alumno: ");
@@ -259,7 +234,7 @@ public class App {
 
         String url = "jdbc:mysql://localhost:3306/instituto";
         String user = "root";
-        String password = "Med@c";
+        String password = "12345";
 
         String sql = "INSERT INTO alumnos (nombre, curso, nota) VALUES (?, ?, ?)";
         try {
@@ -267,7 +242,7 @@ public class App {
             PreparedStatement stmt = conn.prepareStatement(sql);
             ResultSet rs = stmt.executeQuery();
 
-            stmt.setString(1, nombre);// PA PODER AÑADIR LO QUE HEMOS PUESTO ANTES (PARAMETROS Y SU ORDEN AL PONERLOS)
+            stmt.setString(1, nombre);
             stmt.setString(2, curso);
             stmt.setDouble(3, nota);
 
@@ -285,23 +260,17 @@ public class App {
         }
     }
 
-    /*
-     * 7 Actualizar nota tras la recuperación o subida de nota: la séptima opción
-     * consiste en pedir al usuario un ID de un alumno y la nueva nota que ha
-     * obtenido, modificando la nota al alumno correspondiente en la tabla de la
-     * BBDD.
-     */
     public static void actualizarNota() {
-        Scanner sc = new Scanner(System.in);// VALORES PA PONER
-        System.out.print("Dame el ID (alumno): ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Dame el ID (del alumno): ");
         int id = sc.nextInt();
 
-        System.out.print("Pon su nueva nota (X.Y): ");
+        System.out.print("Pon la nueva nota (del alumno): ");
         double nuevaNota = sc.nextDouble();
 
         String url = "jdbc:mysql://localhost:3306/instituto";
         String user = "root";
-        String password = "Med@c";
+        String password = "12345";
 
         String sql = "UPDATE alumnos SET nota = ? WHERE id = ?";
 
@@ -324,18 +293,14 @@ public class App {
         }
     }
 
-    /*
-     * 8 Baja de un alumno: la octava opción del menú consiste en solicitar un ID y
-     * borrar dicho alumno de la BBDD.
-     */
-    public static void DarBaja() {
+    public static void Baja() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Dame el ID del alumno que vamos a Borrar: ");
+        System.out.print("ID del alumno: ");
         int id = sc.nextInt();
 
         String url = "jdbc:mysql://localhost:3306/instituto";
         String user = "root";
-        String password = "Med@c";
+        String password = "12345";
 
         String sql = "DELETE FROM alumnos WHERE id = ?";
 
@@ -345,7 +310,7 @@ public class App {
 
             stmt.setInt(1, id);
 
-            int filas = stmt.executeUpdate();// EJECUTAR EL BORRADO
+            int filas = stmt.executeUpdate();
             System.out.println("Filas borradas: " + filas);
 
             stmt.close();
@@ -356,4 +321,5 @@ public class App {
             System.out.println("Error desconocido " + e.getMessage());
         }
     }
+
 }
